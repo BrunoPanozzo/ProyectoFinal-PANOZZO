d751566de56fe2d40184039bd1ffe26b8058c86f
BrunoPanozzo <brunopanozzo@hotmail.com>
2023-09-28 21:00:36 -0300

    ajustes en el carrito

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,15 +2,21 @@ import { useContext } from 'react'
 import { CartContext } from '../context/CartContext'
 import CartItem from './CartItem'
 import '../styles/cart.css'
+import { Link } from "react-router-dom";
 
 const Cart = () => {
 
-    const { cart, clearCart, totalItems, totalMonto } = useContext(CartContext)    
+    const { cart, clearCart, totalItems, totalMonto } = useContext(CartContext)
 
     const cantidadArticulosComprados = totalItems()
-
-    if (cantidadArticulosComprados == 0)
-        return <h1 className="titulo-importante">No existen productos en el carrito.</h1>
+    
+    if (cantidadArticulosComprados === 0)
+        return (
+            <>
+                <h1 className="titulo-importante">No existen productos en el carrito.</h1>
+                <Link to="/" className="btn btn-secondary text-decoration text-center btn-ver-detalle">Volver a la Tienda</Link>
+            </>
+        )
 
     return (
         <div>
@@ -21,6 +27,7 @@ const Cart = () => {
                 ))}
             </div>
             <p className="cart-resumen-compra">Ud. compró un total de {cantidadArticulosComprados} artículos por un monto total de {totalMonto()}</p>
+            <button onClick={() => clearCart()}>Vaciar carrito</button> 
         </div>
     )
 }
--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -9,7 +9,7 @@ const CartWidget = () => {
     return (
         <div>
             {/* <button type="button" className="btn btn-ligth position-relative" width="20" height="20"> */}
-                <Link to={"/cart/"}>
+                <Link to={"/cart"}>
                     <img src={"../img/NavBar/carrito.png"} alt="Logo de carrito de compras" width="50" height="50" />
                     <span id="cantidadProductos" className="position-absolute translate-middle badge rounded-pill bg-danger">{totalItems()}</span>
                 </Link>
